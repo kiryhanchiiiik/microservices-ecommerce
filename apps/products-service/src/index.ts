00871ba20e1f1617e35355a6fd779807b2245463
kiryhanchiiiik <kirillsabelkijk@gmail.com>
2025-12-17 12:07:26 +0200

    add TestPage for checking backend status

--- apps/products-service/src/index.ts
+++ apps/products-service/src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { clerkMiddleware, getAuth } from "@clerk/express";
 
 const app = express();
 
@@ -10,12 +11,25 @@ app.use(
   })
 );
 
+app.use(clerkMiddleware());
+
 app.get("/health", (req: Request, res: Response) => {
   return res.status(200).json({
     status: "ok",
     uptime: process.uptime(),
     timestamp: Date.now(),
   });
+});
+
+app.get("/test", (req: Request, res: Response) => {
+  const auth = getAuth(req);
+  const userId = auth.userId;
+
+  if (!userId) {
+    return res.status(401).json({ message: "You are not logged in!" });
+  }
+
+  res.json({ message: "Product service authenticated" });
 });
 
 app.listen(8000, () => console.log("Product service is running on port 8000"));
