a98817a6675a2325708f93365cfa04f6f09783b9
kiryhanchiiiik <kirillsabelkijk@gmail.com>
2025-12-18 10:59:40 +0200

    add middleware with auth to payment-service

--- apps/payment-service/src/index.ts
+++ apps/payment-service/src/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { serve } from "@hono/node-server";
+import { shouldBeUser } from "./middleware/authMiddleware.js";
 
 const app = new Hono();
 app.use("*", clerkMiddleware());
@@ -13,17 +14,10 @@ app.get("/health", (c) => {
   });
 });
 
-app.get("/test", (c) => {
-  const auth = getAuth(c);
-
-  if (!auth?.userId) {
-    return c.json({
-      message: "You are not logged in.",
-    });
-  }
-
+app.get("/test", shouldBeUser, (c) => {
   return c.json({
     message: "Payment service is authenticated",
+    userId: c.get("userId"),
   });
 });
 
